c685683d54b2963c124ef987ba145bf36a908564
Maria <maria@gmail.com>
2022-02-16 17:29:26 +0300

    Сохранено

---
+++ AveMaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AveMaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
